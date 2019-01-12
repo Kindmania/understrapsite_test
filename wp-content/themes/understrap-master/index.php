a543048e2e6e77f7117addb87f820cb1c5a869fd
Atsuhiko Narita <atsuhiko.narita@narita.co.jp>
2019-01-12 13:49:47 +0900

    フロントページ振り分け

--- wp-content/themes/understrap-master/index.php
+++ wp-content/themes/understrap-master/index.php
@@ -19,9 +19,7 @@ get_header();
 
 $container = get_theme_mod( 'understrap_container_type' );
 ?>
-<?php if ( is_front_page() ) : ?>
-	<p>フロントページ</p>
-<?php endif; ?>
+
 <?php if ( is_front_page() ) : ?>
 	<?php get_template_part( 'global-templates/hero' ); ?>
 <?php endif; ?>
